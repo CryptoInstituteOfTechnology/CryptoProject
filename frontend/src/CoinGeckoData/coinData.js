6a9bfd795a2ffeaf141b68cc4da737e7743e1cfc
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-25 14:50:40 -0700

    how to fetch request to coinGecko and json file for the string i need + API Key in Files

---
+++ frontend/src/CoinGeckoData/coinData.js
@@ -0,0 +1,6 @@
+// coinNames has the data in the way its suppposed to be fetched from the api
+
+//have to use httpie
+// http GET 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=%20' \
+  // accept:application/json \
+  // x-cg-demo-api-key:'	CG-grzK8TdUSCCcQ2h81fpX2nZn'
