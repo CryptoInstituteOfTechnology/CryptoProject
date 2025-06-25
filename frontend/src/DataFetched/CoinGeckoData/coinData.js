207803719f6bc77f90f8c2c22f25950e4288b104
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-25 14:56:00 -0700

    Switched file structure to have folder that fetches data and draw API Key from coinData

--- frontend/src/DataFetched/CoinGeckoData/coinData.js
+++ frontend/src/DataFetched/CoinGeckoData/coinData.js
@@ -1,6 +1,13 @@
 // coinNames has the data in the way its suppposed to be fetched from the api
+//getting api key from env
+
+API_KEY = meta.env.VITE_COIN_GECKO_MARKET_DATA_API_KEY
+
+
 
 //have to use httpie
 // http GET 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=%20' \
   // accept:application/json \
   // x-cg-demo-api-key:'	CG-grzK8TdUSCCcQ2h81fpX2nZn'
+
+
