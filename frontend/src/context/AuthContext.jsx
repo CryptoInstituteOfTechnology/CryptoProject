39c5296e65544a430e08eba54537413588b39cbd
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-26 10:39:10 -0700

    signup routing

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,37 +3,51 @@ import { supabase } from "../supabaseClient";
 
 // the point is to share state of what session the current user is in, if they are logged in, and their info 
 // how you know who is who in application and give permissions
+//followed tutorial, naming may be unconvetional and unlike rest of project
 const AuthContext = createContext()
 
-export const AuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({ children }) => {
 
-    const[session,setSession] = useState(undefined)
+    const [session, setSession] = useState(undefined)
 
 
     //signup function
     //async dont want to wait for user to be processed befoe continuing
-    const signUpNewUser = async() =>{
+    const signUpNewUser = async () => {
 
         // send supabase a new user to sign up
-        const[data,error] = await supabase.auth.signUp({
+        const [data, error] = await supabase.auth.signUp({
             email: email,
             password: password,
         })
 
-        if(error){
+        if (error) {
             console.error("there was a problem signing up", error)
-            return({success: false, error})
+            return ({ success: false, error })
         }
-        return {success:true, data} //return that data was processed
+        return { success: true, data } //return that data was processed
+    }
 
+    //listen  for state change, only run once when page is loaded, loads what type of session user is in
+    useEffect(() => {
+        superbase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
 
+        // _event because its ignored, we just need one because obj expects one
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
+
+    const signOut = () =>{
 
     }
 
     //login function
 
-    return(
-        <AuthContext.Provider value = {{session, signUpNewUser}}>
+    return (
+        <AuthContext.Provider value={{ session, signUpNewUser }}>
             {children}
         </AuthContext.Provider>
     )
@@ -41,6 +55,6 @@ export const AuthContextProvider = ({children}) => {
 }
 
 
-export const UserAuth = () =>{
+export const UserAuth = () => {
     return useContext(AuthContext)
 }
