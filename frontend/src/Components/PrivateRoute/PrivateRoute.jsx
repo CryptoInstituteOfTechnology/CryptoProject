9913ad74c7ff323f3faccb83297288692b17b8bd
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-27 15:32:54 -0700

    fixed routing issue

--- frontend/src/Components/PrivateRoute/PrivateRoute.jsx
+++ frontend/src/Components/PrivateRoute/PrivateRoute.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({children}) =>{
+const PrivateRoute = ({ children }) => {
     //navigates users to singup if they arent in a session(not logged in) they cant access protected routes
-    const {session} = UserAuth
-   
+    const { session } = UserAuth()
 
-    return(
+    const handleLogin = async () => {
+        // login logic here...
+        if (session) {
+            navigate("/dashboard");
+        }
+    };
 
-        <>{session ? <>{children}</> : <Navigate to = "/signup"/>} </>
+    return (
+
+        <>{session ? <>{children}</> : <Navigate to="/signup" />} </>
     )
 }
 
