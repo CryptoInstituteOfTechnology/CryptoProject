6e9b278d02a2ff08281c4a8e6f56c0c55924658d
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-30 15:29:10 -0700

    all tickers done, news next

--- frontend/src/Components/AllTickersView/AllTickersView.jsx
+++ frontend/src/Components/AllTickersView/AllTickersView.jsx
@@ -2,20 +2,22 @@ import TickerBox from '../TickerBox/TickerBox.jsx'
 import { webFetchedContext } from "../../context/Webfetching/WebFetchContext";
 import { useContext } from 'react';
 import { Table, TableRow, TableCaption, TableBody } from "../ui/table"
+import {ScrollArea} from "../ui/scroll-area.jsx"
 const AllTickersView = () => {
 
 
     const { coinApiData, websocketData } = useContext(webFetchedContext) // get the data
     //map the price to the coindata using the map key from webfetched context, pass it down as
     return (
-        <Table>
+        <Table className= "border-4 border-black">
+            <ScrollArea  className="h-[600px]  rounded-md border p-4">
             <TableCaption>All Crypto Assets</TableCaption>
             <TableBody>
                 {coinApiData.map((coin) => {
                     const websocketPrice = websocketData[coin.id.toLowerCase()]
                     return (
 
-                        <TableRow>
+                        
 
                             <TickerBox
                                 key={coin.id}
@@ -23,13 +25,13 @@ const AllTickersView = () => {
                                 livePrice={websocketPrice}
                             />
 
-                        </TableRow>
+                       
                     )
                 })
 
                 }
             </TableBody>
-
+            </ScrollArea>
         </Table>
     )
 }
--- frontend/src/Components/TickerBox/TickerBox.jsx
+++ frontend/src/Components/TickerBox/TickerBox.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect, useRef } from 'react'
-import { TableCell } from '../ui/table'
+import { TableCell, TableRow } from '../ui/table'
 
 
 //data is passed down from each box to TickerBox
@@ -13,7 +13,7 @@ export default function TickerBox({ coinData, livePrice }) {
 
     //need a last price variable so you can turn red or green later
     //qauantity variable as well
-    const [quantity, setQuantity] = useState(0)
+   
     const [priceColor, setPriceColor] = useState('text-white') // track what color text will be
     const previousPrice = useRef(null) // useref so we dont lost the value when reloaded
 
@@ -42,21 +42,12 @@ export default function TickerBox({ coinData, livePrice }) {
         //     //api call to add
     }
 
-    //have to have logic to check if in watchlist alrready
-
-
-
-    // //function called addtoPortoflio, makes api call to add to portfolio thing on backend
-    const addToPortfolio = (e) => {
-        console.log(`just bought ${dataStreamed.s} at ${dataStreamed.p} with ${quantity} dshares!`)
-        //     //add to portfolio in tuple way (Name, Price)
-    }
-    // //need logic to check if in portfolio alr and if it is add more shares
+    //function to open a modal to trade stocks
 
 
 
     return (
-        <div className="ticker-container">
+        <TableRow className = "border-4 border-black">
             <TableCell>
                 <img src= {coinData.image} alt = {coinData.symbol}  style={{ width: '50px', height: 'auto' }} />
             </TableCell>
@@ -68,19 +59,14 @@ export default function TickerBox({ coinData, livePrice }) {
             <TableCell>High: {coinData.high_24h}</TableCell>
             <TableCell>Low: {coinData.low_24h}</TableCell>
             <TableCell>
-                <button onClick={addToWatchlist}> Add to Watchlist</button>
+                <button onClick={addToWatchlist} className='flex items-center gap-1 hover:bg-yellow-300  transition duration-300 ease-in-out rounded-lg'> 
+                    <h2>Add to Watchlist </h2>
+                    <h2 className="text-yellow-600">★</h2>
+                </button>
             </TableCell>
-            <TableCell>
-                <form onSubmit={addToPortfolio}>
-                    <input
-                        type="number"
-                        value={quantity}
-                        onChange={(event) => setQuantity(event.target.valueAsNumber)} // Check that is a postiive number 
-                        placeholder="Enter quantity"
-                    />
-                    <button type="submit">Add to Portfolio</button>
-                </form>
+            <TableCell className="text-right">         
+                    <button className='hover:bg-yellow-300 rounded-lg transition duration-300 ease-in-out' type="submit">Add to Portfolio</button>
             </TableCell>
-        </div>
+        </TableRow>
     )
 }
