b4b83b7443eea8b666cc0b842355c3fe95c01980
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-30 17:57:31 -0700

    Have to fix navbar link issues and table format for all tickers view, next after is to do news and assetview then mvoe to backend

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,65 @@
-import React from "react";
+// import React from "react";
+// import { NavigationMenu, NavigationMenuList, NavigationMenuItem} from "../ui/navigation-menu"
+// import { Link } from "react-router-dom";
+// export default function NavBar() {
 
-export default function NavBar (){
-    return(
-        <div>
-            
-        </div>
-    )
-}
+
+//     //neeed to add redirecting for search for website that takes them to assetview
+//     return (
+//         <div className="justify-center container mx-auto p-4">
+//             <NavigationMenu className="bg-blue-500  border-4 rounded-md w-full max-w-7xl font-bold flex-shrink ">
+//                 <NavigationMenuList >
+
+
+
+
+
+
+
+
+
+//                     <NavigationMenuItem>
+//                         <Link to="/dashboard">DashBoard</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/alltickers">All Tickers</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/news">News</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem className="border-4 rounded-md border-black text-black bg-white">
+//                         <form>
+//                             <input classname="text-black placeholder:text-xs " type="search" placeholder="Search Ticker(ex: BTC)" />
+//                             <button type="submit">Search</button>
+//                         </form>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/watchlist">Watchlist</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/portfolio">Portfolio</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/recommendations">Recommendations</Link>
+//                     </NavigationMenuItem>
+
+//                     <NavigationMenuItem>
+//                         <Link to="/login">Login</Link>
+//                     </NavigationMenuItem>
+
+
+//                     <NavigationMenuItem>
+//                         <Link to="/signup">Signup</Link>
+//                     </NavigationMenuItem>
+
+//                 </NavigationMenuList>
+//             </NavigationMenu>
+//         </div>
+//     )
+// }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Components/Router/router.jsx'
-import NavBar from './Components/NavBar/NavBar.jsx'
+// import NavBar from './Components/NavBar/NavBar.jsx'
 import Footer from './Components/Footer/Footer.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import WebFetchContextProvider from './context/Webfetching/WebFetchContext.jsx'
@@ -12,13 +12,15 @@ import WebFetchContextProvider from './context/Webfetching/WebFetchContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
-      <NavBar />
       <AuthContextProvider>
+        {/*<NavBar /> */}
         <WebFetchContextProvider>
+
           <RouterProvider router={router} />
         </WebFetchContextProvider>
+        <Footer />
       </AuthContextProvider>
-      <Footer />
-      </div>
+
+    </div>
   </StrictMode>,
 )
