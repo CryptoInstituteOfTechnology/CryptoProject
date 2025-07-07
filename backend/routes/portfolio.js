66305655c8767d35a39deb1cbd4f2298145235da
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-07 07:45:13 -0700

    Portfolio shcema done

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,4 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const {PrismaClient} = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient() // same as from kudosboard, DELETE these comments after merged and working in backend folder
+
+// simple get request
+
+
+
+router.get('/:userId', async (req, res) => {
+    const { userID } = req.params
+
+    try {
+        const portfolio = await prisma.PortfolioEntry.findMany({
+            where: {
+                userId,
+                quantity: { gt: 0 } // return ones that arent 0 , 0 means have been bought and sold
+            }, 
+            orderBy :{
+                quantity: 'desc' // display by qunatity in largest first
+            }
+        })
+
+        res.json(portfolio)
+    } catch (error) {
+        res.status(500).json({ error: 'error fetching portfolio ' })
+    }
+})
+
+// on frontend have to have type, send the userid, quantity, price and symbol
