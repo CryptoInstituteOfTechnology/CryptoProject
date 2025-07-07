c79d10214a54dc0b5cc1c3450f4a911ea3affce5
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-06 17:58:52 -0700

    fixed onexit issue

--- frontend/src/Components/AddToPortfolio/AddToPortfolio.jsx
+++ frontend/src/Components/AddToPortfolio/AddToPortfolio.jsx
@@ -9,11 +9,15 @@ export default function AddToPortfolio({ coinData, livePrice }) {
     return (
         <div>
 
-            {showModal && <AddToPortfolioModal />}
+            {showModal && <AddToPortfolioModal
+                coinData={coinData}
+                livePrice={livePrice}
+                onExit={setShowModal(prev => !prev)}
+            />}
             <button
                 className="bg-yellow-200 hover:bg-yellow-400 rounded-lg transition duration-300 ease-in-out py-2 px-4"
                 type="submit"
-                onExit ={() => setShowModal(prev => !prev)} // used this code from old kudosboard proj
+                onClick ={() => setShowModal(true)} // used this code from old kudosboard proj
             >
                 <span className="font-bold">Add to Portfolio</span>
             </button>
