a48d180b9a668a45ee2138577d1757e3ee6e5c75
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-07 17:59:55 -0700

    commit of code before  infite loop of loading for webpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,16 +5,16 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Components/Router/router.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import WebFetchContextProvider from './context/Webfetching/WebFetchContext.jsx'
+import { BackEndContextProvider } from './context/BackEndContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
       <AuthContextProvider>
         <WebFetchContextProvider>
-          <RouterProvider router={router}/>
+            <RouterProvider router={router} />
         </WebFetchContextProvider>
       </AuthContextProvider>
-
     </div>
   </StrictMode>,
 )
