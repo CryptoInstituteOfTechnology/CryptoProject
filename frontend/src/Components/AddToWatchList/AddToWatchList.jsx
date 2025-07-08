8bcdb39a118a97cdc48910a9c98500a6539c30a0
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-08 00:34:55 -0700

    logic for removing and adding to watchlist

--- frontend/src/Components/AddToWatchList/AddToWatchList.jsx
+++ frontend/src/Components/AddToWatchList/AddToWatchList.jsx
@@ -1,20 +1,67 @@
+import { useBackendAttributes } from "../../context/BackEndContext"
 
+const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL
 export default function AddToWatchList({ coinData }) {
+    const { watchlist, fetchWatchlist, userId } = useBackendAttributes()
 
-    const addToWatchlist = (e) => {
-        console.log(`added ${coinData.symbol} to watch list`)
+    //variable check for in watchlist
+
+    const inList = watchlist.includes((entry) => {
+        return entry.symbol.toLowerCase() === coinData.symbol.toLowerCase()
+    })
+    
+
+
+    //toggle watchlist function
+
+    const toggleWatchlist = async (e) => {
+        e.stopPropogation()
+        try {
+            const res = await fetch(`${BACKEND_BASE_URL}/api/watchlist`, {
+
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    userId,
+                    symbol: coinData.symbol.toUpperCase(),
+                }),
+            })
+
+
+            const result = await res.json()
+            console, log(result)
+            await fetchWatchlist() // refetch watchlist to update
+        } catch (error) {
+            console.error(error)
+        }
+
+
     }
 
-    
+
 
     return (
         <div className="flex flex-col gap-2">
             <button
-                onClick={addToWatchlist}
-                className="flex items-center gap-1 bg-yellow-200 hover:bg-yellow-400 transition duration-300 ease-in-out rounded-lg py-2 px-4"
+                onClick={toggleWatchlist}
+                className={
+                    `flex items-center gap-1  transition duration-300 ease-in-out rounded-lg py-2 px-4
+                    ${inList
+                        ? "bg-red-200 hover:bg-red-400"
+                        : "bg-yellow-200 hover:bg-yellow-400"}
+                    `}
             >
-                <span className="text-lg font-bold">Add to Watchlist</span>
-                <span className="text-yellow-600 text-2xl">★</span>
+                <span className="text-lg font-bold">
+                    {inList
+                        ? "Remove From Watchlist"
+                        : "Add To Watchlist"
+                    }
+                </span>
+                <span className="text-yellow-600 text-2xl">
+                    {inList ? "\u2715" : "\u2605"}
+                </span>
             </button>
         </div>
     )
