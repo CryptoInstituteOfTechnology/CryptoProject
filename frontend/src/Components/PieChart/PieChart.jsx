a8c4507949c2681ad31b4839ea99e2797ee8067c
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-14 16:39:17 -0700

    full logic for price pie chart, adding symbol and hover interaction

--- frontend/src/Components/PieChart/PieChart.jsx
+++ frontend/src/Components/PieChart/PieChart.jsx
@@ -1,7 +1,7 @@
 import { useBackendAttributes } from "../../context/BackEndContext";
 
 
-// find percentage makeups of portfolio
+// find percentage makeups of portfolio, and assign random color
 function findWeights(data) {
     let total = 0
     for (const coin of data) {
@@ -9,7 +9,15 @@ function findWeights(data) {
     }
     const mapofWeights = data.map((coin) => {
         const percent = (coin.avgPrice * coin.quantity) / total
-        return [coin.symbol, percent]
+        const red = Math.floor(Math.random() * 156) + 100
+        const green = Math.floor(Math.random() * 156) + 100
+        const blue = Math.floor(Math.random() * 156) + 100
+        const color = `rgb(${red},${blue},${green})`
+        return {
+            symbol: coin.symbol, 
+            percent,
+            color
+        }
     })
     return mapofWeights
 }
@@ -29,29 +37,29 @@ export default function Piechart() {
     //tracker to see what percent has been filled, so you know where to start next slice
     let cumulativePercent = 0;
     //for each slice
-    slices.forEach(slice => {
-        //start and end of x and y coordinates, need to know end of y and x to start next slice
+
+    const paths = slices.map((slice, i) =>{
         const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
         cumulativePercent += slice.percent;
         const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
-        // if the slice is more than 50%, take the large arc (the long way around)
+          // if the slice is more than 50%, take the large arc (the long way around)
         const largeArcFlag = slice.percent > .5 ? 1 : 0;
-
         // create an array and join it just for code readability
         const pathData = [
             `M ${startX} ${startY}`, // Move
             `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
             `L 0 0`, // Line
         ].join(' ');
-        const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        pathEl.setAttribute('d', pathData);
-        pathEl.setAttribute('fill', slice.color);
-        svgEl.appendChild(pathEl);
-    });
+
+        return <path key = {i} d = {pathData} fill = {slice.color}/>
+    })
 
     return (
-        <svg>
-
+        <svg
+            viewBox="-1 1 2 2" // puts center at the center of circle
+            style = {{transform: "rotate(-90deg)"}} // start circle at top
+        > 
+            {paths}
         </svg>
     )
 
