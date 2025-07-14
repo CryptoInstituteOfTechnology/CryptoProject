4f71c7765f258ba3eedf548898a3503abc7004d0
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-14 16:28:13 -0700

    base code for creating pie chart

--- frontend/src/Components/PieChart/PieChart.jsx
+++ frontend/src/Components/PieChart/PieChart.jsx
@@ -2,33 +2,56 @@ import { useBackendAttributes } from "../../context/BackEndContext";
 
 
 // find percentage makeups of portfolio
-function findWeights (data) {
+function findWeights(data) {
     let total = 0
-    for (const coin of data){
+    for (const coin of data) {
         total += (coin.quantity * coin.avgPrice)
     }
-    const mapofWeights = data.map((coin) =>{
-        const percent = (coin.avgPrice * coin.quantity) / total 
-        return[coin.symbol, percent]
+    const mapofWeights = data.map((coin) => {
+        const percent = (coin.avgPrice * coin.quantity) / total
+        return [coin.symbol, percent]
     })
     return mapofWeights
 }
 
+function getCoordinatesForPercent(percent) {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+    return [x, y];
+}
+
 export default function Piechart() {
     const { portfolio } = useBackendAttributes()
-    weightsOfPortfolio = findWeights(portfolio)
 
-    //give it a angle out of 360, thats the weight it gets
+    //get percentages of slice
+    const slices = findWeights(portfolio)
 
-    //logic for a circle idk and give it a color
+    //tracker to see what percent has been filled, so you know where to start next slice
+    let cumulativePercent = 0;
+    //for each slice
+    slices.forEach(slice => {
+        //start and end of x and y coordinates, need to know end of y and x to start next slice
+        const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
+        cumulativePercent += slice.percent;
+        const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
+        // if the slice is more than 50%, take the large arc (the long way around)
+        const largeArcFlag = slice.percent > .5 ? 1 : 0;
 
-    //reload on eevery reload
+        // create an array and join it just for code readability
+        const pathData = [
+            `M ${startX} ${startY}`, // Move
+            `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
+            `L 0 0`, // Line
+        ].join(' ');
+        const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        pathEl.setAttribute('d', pathData);
+        pathEl.setAttribute('fill', slice.color);
+        svgEl.appendChild(pathEl);
+    });
 
-
-
-    return(
+    return (
         <svg>
-            
+
         </svg>
     )
 
