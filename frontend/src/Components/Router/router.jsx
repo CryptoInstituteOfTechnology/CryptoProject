a94c2f82c06a83842612b2e40898600309dc8a99
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-15 12:34:55 -0700

    loading spinner from shadcn in proj

--- frontend/src/Components/Router/router.jsx
+++ frontend/src/Components/Router/router.jsx
@@ -3,6 +3,7 @@ import React, { lazy } from "react";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import { cn } from '@/lib/utils'
 import { Outlet } from "react-router-dom";
 
 const Layout = () => (
@@ -31,21 +32,188 @@ const News = lazyWithDelay(() => import("../../Pages/News/News"));
 const Portfolio = lazyWithDelay(() => import("../../Pages/Portfolio/Portfolio"));
 const Recommendations = lazyWithDelay(() => import("../../Pages/Recommendations/Recommendations"));
 const WatchList = lazyWithDelay(() => import("../../Pages/Watchlist/Watchlist"));
+
+// ui for loading
+export const LoadingSpinner = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="400"
+            height="400"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("animate-spin", className)}
+        >
+            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+    )
+}
+
+
+
 export const router = createBrowserRouter([
     {
         element: <Layout />,
         children: [
-            { path: "/", element: <React.Suspense fallback={<div>Loading...</div>}><App /></React.Suspense> },
-            { path: "/signup", element: <React.Suspense fallback={<div>Loading...</div>}><Signup /></React.Suspense> },
-            { path: "/login", element: <React.Suspense fallback={<div>Loading...</div>}><Login /></React.Suspense> },
+            {
+                path: "/",
+                element: (
+                    <React.Suspense
+                        fallback={
+                            <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                Loading... <LoadingSpinner className="w-12 h-12" />
+                            </div>
+                        }
+                    >
+                        <App />
+                    </React.Suspense>
+                ),
+            },
+            {
+                path: "/signup",
+                element: (
+                    <React.Suspense
+                        fallback={
+                            <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                Loading... <LoadingSpinner className="w-12 h-12" />
+                            </div>
+                        }
+                    >
+                        <Signup />
+                    </React.Suspense>
+                ),
+            },
+            {
+                path: "/login",
+                element: (
+                    <React.Suspense
+                        fallback={
+                            <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                Loading... <LoadingSpinner className="w-12 h-12" />
+                            </div>
+                        }
+                    >
+                        <Login />
+                    </React.Suspense>
+                ),
+            },
             // Protected routes
-            { path: "/dashboard", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><Dashboard /></React.Suspense></PrivateRoute> },
-            { path: "/alltickers", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><AllTickers /></React.Suspense></PrivateRoute> },
-            { path: "/assetview/:symbol", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><AssetView /></React.Suspense></PrivateRoute> },
-            { path: "/news", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><News /></React.Suspense></PrivateRoute> },
-            { path: "/watchlist", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><WatchList /></React.Suspense></PrivateRoute> },
-            { path: "/portfolio", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><Portfolio /></React.Suspense></PrivateRoute> },
-            { path: "/recommendations", element: <PrivateRoute><React.Suspense fallback={<div>Loading...</div>}><Recommendations /></React.Suspense></PrivateRoute> },
+            {
+                path: "/dashboard",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <Dashboard />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/alltickers",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <AllTickers />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/assetview/:symbol",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <AssetView />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/news",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <News />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/watchlist",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <WatchList />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/portfolio",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <Portfolio />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/recommendations",
+                element: (
+                    <PrivateRoute>
+                        <React.Suspense
+                            fallback={
+                                <div className="flex flex-col justify-center items-center h-screen text-xl space-y-4">
+                                    Loading... <LoadingSpinner className="w-12 h-12" />
+                                </div>
+                            }
+                        >
+                            <Recommendations />
+                        </React.Suspense>
+                    </PrivateRoute>
+                ),
+            },
         ],
     },
 ]);
