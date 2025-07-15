6c238e0bc3d4985eeadaafb781e8fa99837ec67d
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-15 14:28:49 -0700

    added backend route to get portfolio articles

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use('/api/portfolio' , require('./routes/portfolio'))
 app.use('/api/watchlist' , require('./routes/watchlist'))
 app.use('/api/transactions' , require('./routes/transactions'))
-app.use('api/portfolionews',require('./routes/news'))
+app.use('/api/portfolionews',require('./routes/news'))
 
 app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`)
