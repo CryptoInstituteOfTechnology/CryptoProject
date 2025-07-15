f663ac67c24b17b7ff346f977471f89e9742fd30
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-15 14:16:41 -0700

    article and symbol logic

---
+++ backend/routes/news.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+
+router.get('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    // get portfolio entries
+    const portfolio = await prisma.portfolioEntry.findMany(
+        {
+            where: { userId }
+        }
+    )
+    const userSymbols = portfolio.map(entry => entry.symbol)
+
+
+    //only get 15 articles, 15 articles is on average the amount for past 2 cron jobs
+    const articles = await prisma.article.findMany({
+        take: 15
+    })
+
+    //map out symbols
+    const symbolArticleMap = new Map()
+    for (const article of articles) {
+        for (const symbol of article.symbols) {
+            if (!symbolArticleMap.has(symbol)) {
+                //create a new key map pairing
+                symbolArticleMap.set(
+                    symbol, []
+                )
+            }
+            symbolArticleMap.get(symbol).push(article)
+        }
+    }
+
+    // find articles that match the symbols in portfolios
+    const matchedArticles = new Set()
+
+    for (const symbol of userSymbols) {
+        if (symbolArticleMap.has(symbol)) {
+            symbolArticleMap.get(symbol).forEach((article) => {
+                matchedArticles.add(article)
+            })
+        }
+    }
+
+    res.json(matchedArticles)
+
+
+})
+
+module.exports = router;
