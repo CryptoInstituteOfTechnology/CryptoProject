21eed856ae77b47f6c72310726595d5276e709e1
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-17 16:28:32 -0700

    needed symbols

---
+++ ml/utils/buildvectors.py
@@ -0,0 +1,14 @@
+# given a portfolio build a vector
+#store weights in a dictionary key vlaue pair
+crypto_symbols = [
+    "BTC", "ETH", "BNB",
+    "LTC", "XRP", "EOS", 
+    "TRX", "ADA", "XMR", 
+    "DASH","ZEC", "QTUM",
+    "IOTA", "NEO", "ARB", 
+    "VET", "ONT", "LINK", 
+    "XTZ", "RVN","DOGE",
+    "SOL", "DOT", "FTM", 
+    "XLM", "SUI", "NEAR"
+]
+
