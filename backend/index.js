f0c69f05a1a661c8aac3abe0d2400d6bf989ae0e
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-18 15:43:04 -0700

    historic points for charts

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use('/api/portfolio' , require('./routes/portfolio'))
 app.use('/api/watchlist' , require('./routes/watchlist'))
 app.use('/api/transactions' , require('./routes/transactions'))
+app.use('api/stats', require('./routes/stats') )
 
 app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`)
