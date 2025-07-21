94592947e4b096b72b3d898d98a8e8c3c4deb22e
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-21 00:57:19 -0700

    test file changes

--- ml/main.py
+++ ml/main.py
@@ -1,10 +1,10 @@
-from routing.supabasewrapper import SupabaseAPIWrapper
+from tests.mockAPIWrapper import SupabaseAPIWrapperMock
 from generate_recommendations import generate_recommendations_for_users
 
 
 if __name__ == "__main__":
     base_url = "http://localhost:5000"
-    api = SupabaseAPIWrapper(base_url)
+    api = SupabaseAPIWrapperMock(base_url)
     print("running rec model")
     generate_recommendations_for_users(api)
     print("done")
