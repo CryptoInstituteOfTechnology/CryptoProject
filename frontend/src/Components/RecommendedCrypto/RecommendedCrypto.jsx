d4a40034f1a7e8b59bfadab8062b87b67412ab88
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-21 22:29:40 -0700

    imports for recs

--- frontend/src/Components/RecommendedCrypto/RecommendedCrypto.jsx
+++ frontend/src/Components/RecommendedCrypto/RecommendedCrypto.jsx
@@ -1,4 +1,13 @@
-export default function RecommendedCrypto(){
+import { useBackendAttributes } from "../../context/BackEndContext";
+import { webFetchedContext } from "../../context/Webfetching/WebFetchContext";
+import { useContext } from 'react';
+
+// return a list of cryptos w their symbols and updating prices
+export default function RecommendedCrypto() {
+    const { websocketData, coinApiData } = useContext(webFetchedContext)
+    const { recommendations } = useBackendAttributes()
+
+
     return (
         <div>
             Recommended Crypto
