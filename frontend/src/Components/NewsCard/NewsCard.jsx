0eeb2542676843e0fd41b4afd28ce477ddaeb798
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-24 14:52:42 -0700

    added fixes to news lgihting, images on tickers, reloaded all time statistics, cofirm sell red

--- frontend/src/Components/NewsCard/NewsCard.jsx
+++ frontend/src/Components/NewsCard/NewsCard.jsx
@@ -3,13 +3,13 @@ import { TableCell, TableRow } from '../ui/table';
 
 const NewsCard = ({ news }) => {
   return (
-    <TableRow className="hover:shadow-lg transition-shadow hover:bg-gray-800 bg-gray-900 border border-gray-700 rounded-md mb-2">
+    <TableRow className="hover:shadow-lg transition-shadow hover:bg-blue-800 bg-blue border border-gray-700 rounded-md mb-2">
       <TableCell className="p-4">
         <a
           href={news.URL}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center text-white hover:text-blue-400"
+          className="flex items-center text-white "
         >
           <img src={news.IMAGE_URL} alt="image of news" className="w-24 h-auto mr-4 rounded" />
           <div className="text-lg font-bold">
