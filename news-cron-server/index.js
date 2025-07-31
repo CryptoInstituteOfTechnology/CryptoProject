df40a1390633c47ae3ceb5b0784d2ccb71b807b7
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-31 14:07:07 -0700

    Change for cron jobs

--- news-cron-server/index.js
+++ news-cron-server/index.js
@@ -1,11 +1,31 @@
 const sendEmails = require('./sendmail/sendmail')
 const parseArticles = require('./parseArticles/parseArticles')
 const sentimentalArticles = require('./sentiment/sentiment-analysis')
+const cron = require('node-cron')
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// use this to keep pinging render service
+app.get('/', (_, res) => {
+    res.send('News Cron Job Server is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
 
 async function main() {
-    const articles = await sentimentalArticles();
-    await parseArticles(articles);
-    await sendEmails();
+    console.log('Running cron job at', new Date().toISOString());
+
+    try {
+        const articles = await sentimentalArticles();
+        await parseArticles(articles);
+        await sendEmails();
+        console.log('Cron job done');
+    } catch (err) {
+        console.error('Error in cron job:', err);
+    }
 }
 
 main();
