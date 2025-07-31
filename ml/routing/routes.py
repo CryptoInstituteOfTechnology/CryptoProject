ed9f3497bc759a4c1b9f6d2d79dc8fb493bd81bf
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-07-31 16:05:16 -0700

    debugging removal

--- ml/routing/routes.py
+++ ml/routing/routes.py
@@ -108,4 +108,4 @@ def post_recommendations():
     return jsonify({"message": f"{len(data)} recommendations added"}), 201
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
